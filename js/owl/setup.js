42fab88965ff334229e289cd685cf836f6a27fd6
Mauro Caetano Rodrigues <68354696+maurocr@users.noreply.github.com>
2020-12-22 21:40:21 -0300

    upload do arquivo setup.js
    Alterei as seguintes configurações: Desabilitei o efeito de loop; alterei para 20px a margem de um elemento para outro; ativei as duas setas de navegação nos elementos; alterei a responsividade para exibir numa tela de tamanho entre 600px a 1000px 4 elementos e numa tela acima 1000px 6 elementos.

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
+    loop:false,
+    margin:20,
+    nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:3
+            items:4
         },
         1000:{
-            items:5
+            items:6
         }
     }
 })
